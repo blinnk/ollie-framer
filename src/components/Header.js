6d633e4f481317ecc9dbc00892da72016f3f9002
willgr <willfromspace@gmail.com>
2021-05-28 23:52:37 -0700

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React from 'react'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header-inner">
-        <div className="logo">Ollie</div>
-        <nav className="nav">
+    <motion.div
+      className='header'
+      initial={{ opacity: 0, y: -180 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1,
+        delay: 0.6
+      }}
+    >
+      <div className='header-inner'>
+        <div className='logo'>Ollie</div>
+        <nav className='nav'>
           <li>
-            <a href="/design">Design</a>
+            <a href='/design'>Design</a>
           </li>
           <li>
-            <a href="/strategy">Strategy</a>
+            <a href='/strategy'>Strategy</a>
           </li>
           <li>
-            <a href="/cases">Cases</a>
+            <a href='/cases'>Cases</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href='/about'>About</a>
           </li>
           <li>
-            <a href="/why">Why work with us?</a>
+            <a href='/why'>Why work with us?</a>
           </li>
         </nav>
-        <div className="contact">
-          <a href="/contact">Let's work together</a>
+        <div className='contact'>
+          <a href='/contact'>Let's work together</a>
         </div>
-        <div className="hamburger-menu">
+        <div className='hamburger-menu'>
           <span></span>
           <span></span>
         </div>
       </div>
-    </div>
-  );
-};
+    </motion.div>
+  )
+}
 
-export default Header;
+export default Header
